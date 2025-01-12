688162f86d8873fd6909c53c242722bcbe2ca9c8
Yuta Tokoi <yutatokoi@proton.me>
2025-01-12 16:44:47 +0900

    dynamic route handling

--- main.go
+++ main.go
@@ -4,16 +4,30 @@ import (
     "fmt"
     "log"
     "net/http"
+    "path/filepath"
 )
 
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+    requestedPath := r.URL.Path
+    if requestedPath == "/" {
+        requestedPath = "index.html"
+    } else {
+        requestedPath += ".html"
+    }
+
+    filePath := filepath.Join("./templates", requestedPath)
+
+    http.ServeFile(w, r, filePath)
+}
+
 func main() {
-    templatesFileServer := http.FileServer(http.Dir("./templates"))
-    http.Handle("/", templatesFileServer)
+
+    http.HandleFunc("/", serveTemplate)
 
     staticFileServer := http.FileServer(http.Dir("./static"))
     http.Handle("/static/", http.StripPrefix("/static/", staticFileServer))
 
-    fmt.Printf("Starting server at port 8080\n")
+    fmt.Println("Starting server at port 8080")
 
     if err := http.ListenAndServe(":8080", nil); err != nil {
         log.Fatal(err)
